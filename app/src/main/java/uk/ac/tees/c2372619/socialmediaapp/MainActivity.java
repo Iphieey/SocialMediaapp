7a31bc02285c71ccbc1b5ab83317161d74791e19
Iphieey <iphiemoks@gmail.com>
2023-04-24 21:08:21 +0100

    ICA Mobile application development social media app

---
+++ app/src/main/java/uk/ac/tees/c2372619/socialmediaapp/MainActivity.java
@@ -0,0 +1,36 @@
+package uk.ac.tees.c2372619.socialmediaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button register = findViewById(R.id.register);
+        Button login = findViewById(R.id.login);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this , LoginActivity.class));
+                finish();
+            }
+        });
+    }
+}
