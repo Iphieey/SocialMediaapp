b0bd969980fccb38f2248c8b057a1181d35a6e95
Iphieey <iphiemoks@gmail.com>
2023-04-26 07:00:53 +0100

    New commit 2

--- app/src/main/java/uk/ac/tees/c2372619/socialmediaapp/MainActivity.java
+++ app/src/main/java/uk/ac/tees/c2372619/socialmediaapp/MainActivity.java
@@ -2,35 +2,29 @@ package uk.ac.tees.c2372619.socialmediaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button register = findViewById(R.id.register);
-        Button login = findViewById(R.id.login);
+        logout = findViewById(R.id.logout);
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
-                finish();
-            }
-        });
-
-        login.setOnClickListener(new View.OnClickListener() {
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this , LoginActivity.class));
-                finish();
+                FirebaseAuth.getInstance().signOut();
             }
         });
+
     }
 }
---
+++ app/src/main/java/uk/ac/tees/c2372619/socialmediaapp/StartActivity.java
@@ -0,0 +1,36 @@
+package uk.ac.tees.c2372619.socialmediaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button register = findViewById(R.id.register);
+        Button login = findViewById(R.id.login);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this , LoginActivity.class));
+                finish();
+            }
+        });
+    }
+}
