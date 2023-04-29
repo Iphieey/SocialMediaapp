9092ebe6b46c56f2a766454b09b460935ecbcf8e
Iphieey <iphiemoks@gmail.com>
2023-04-29 12:29:02 +0100

    cloud store

--- app/src/main/java/uk/ac/tees/c2372619/socialmediaapp/MainActivity.java
+++ app/src/main/java/uk/ac/tees/c2372619/socialmediaapp/MainActivity.java
@@ -1,18 +1,27 @@
 package uk.ac.tees.c2372619.socialmediaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,5 +58,21 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        DocumentReference documentReference = FirebaseFirestore.getInstance().collection("favourites").document("EDO");
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if (documentSnapshot.exists()){
+                        Log.d("Document" , documentSnapshot.getData().toString());
+                    }else {
+                        Log.d("Document" , "No data");
+                    }
+                }
+            }
+        });
+
     }
 }
